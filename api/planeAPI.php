af0e3644f77918fa847904a27d8a85c6fb80d8d0
omer.zfira@gmail.com <omer.zfira@gmail.com>
2017-01-02 09:36:42 +0200

    init git

---
+++ api/planeAPI.php
@@ -0,0 +1,43 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+require_once '../util/util.php';
+require_once '../model/plane.php';
+
+dbConnect();
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+     
+     $idToGet = (int) r('id');
+
+     if ($idToGet) {
+        $plane = getPlaneById($idToGet);
+        echo json_encode($plane);
+     } else {
+        $planes = getPlanes();
+        echo json_encode($planes);
+     }
+
+} else if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+     $idToUpdate = (int)$_REQUEST['id'];
+
+    // Read the JSON we got in the req
+    //test git 
+    $entity = file_get_contents('php://input');
+    $entity = json_decode($entity);
+    updatePlane($entity);
+
+} else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+     $idToRemove = (int)$_REQUEST['id'];
+
+     deletePlaneById($idToRemove);
+
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $entity = file_get_contents('php://input');
+    $entity = json_decode($entity);
+   
+    insertPlane($entity);
+}
+
+$conn->close();
+?>
